784a5d307b4fea7ca7bfd760a400774b9db4a6f5
Wania Chaudhary <cocogujjar786@gmail.com>
2026-07-25 01:22:11 +0500

    add User_Input folder

---
+++ 05_User_Input/user_input.py
@@ -0,0 +1,11 @@
+a=input("Enter a your Name:") # get input from user always in a string data type if a number and character value in a input function
+print(a)
+age=input("Enter a age:") 
+print(age)
+
+#type casting in user input
+# typecasting if used you want a another datatype like integer and float
+a=int(input("Enter a value of a:"))
+b=int(input("Enter a value of b:"))
+sum=a+b
+print(sum)
