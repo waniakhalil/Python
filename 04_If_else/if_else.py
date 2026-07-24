389c7f850739c9fe7c9922cccd89b02c2b76b035
Wania Chaudhary <cocogujjar786@gmail.com>
2026-07-25 00:32:17 +0500

    status

--- 04_If_else/if_else.py
+++ 04_If_else/if_else.py
@@ -1,7 +1,19 @@
-a = 100
-b = 200
+a = 200
+b = 100
 
-if a > b:
+ # logical operator is used to compare two values and return a boolean value True or False
+ # : is used to start a block of code
+ # Indentation means leaving spaces at the beginning of a line in a block 
+# Indentation usually uses 4 spaces or 1 tab this tells python that the code is part of the if statement
+# if the condition is true, the code inside the if block will be executed and if is always true statement
+# if the condition is false, the code inside the else block will be executed
+
+if a>b: #retu#rn true 
     print("a is greater than b")
-else:
+    print("Hello A")
+    print("Hello B")
+print("Hello C")
+
+if b>a: #return false
     print("b is greater than a")
+  
