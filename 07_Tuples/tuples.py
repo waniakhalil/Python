4344a279244ac8b535fb33231b2a39c6af8b953d
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-07-28 01:07:01 +0500

    adds folder 07_Tuples

---
+++ 07_Tuples/tuples.py
@@ -0,0 +1,27 @@
+# Tuples 
+#one or more elements or bunch of element store like a list second way to store a more elements in a single variable
+# but one major difference between tuple and list
+# Not add, delete ,remove ,clear function not perform on tuple this function perform on list
+#  in tuple value or element is constant and in list value or element is temporary you can change in it 
+# list is mutable(change) and tuple is immutable (not change)
+# in list is slcicing not in tuple
+
+# tuples in function
+from itertools import count
+
+
+name=('wania','husnain','sawera',20,18,16)
+# syntax:varibale name=()round bracket define a tuple and [] square bracket define  a list
+
+# access
+print(name[2])
+#length 
+print(len(name))
+
+# count
+print(name.count(20))
+
+#index
+print(name.index('wania'))
+
+# these function deletails and syntax are define in list file .py 
