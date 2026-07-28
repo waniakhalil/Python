ccfbf1f438260673e7613e6adb9c2fa664ca4ba6
waniakhalil <waniakhalilchaudhary@gmail.com>
2026-07-29 01:23:40 +0500

    adds the folder 08_Loops

---
+++ 08_Loops/for_loop.py
@@ -0,0 +1,15 @@
+# Loop
+# Any task repeated or repeat  task is called loop
+# if one code write many times is solution is loop
+# for example : if your task is print you name 50 times you code write 50 times ir time confusing and not flexiable and not fe
+# is solution is loop
+# loop have starting and ending point  tells how many times loop run
+# loop execute task perform
+# python have 2 loops 
+# 1.for loop
+# 2.while loop
+# for loop syntax:
+# for elem in( list name) :
+#: indicates the block of loop
+# break use keyword in loop fo terminate and exit the loop
+# coutinue use keyword skip the loop 
